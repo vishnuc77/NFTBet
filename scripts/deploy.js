1d840d6946db27772b26d11a70866fffbaf07308
swathireddy26 <swathireddyg54@gmail.com>
2022-01-16 14:00:57 +0530

    Adds Staking contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,38 +1,21 @@
-const fs = require("fs");
-const { NFTStorage, Blob } = require("nft.storage");
+const main = async () => {
+  const [deployer] = await ethers.getSigners();
 
-const client = new NFTStorage({
-  token:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDAxOTQwOTkwZmMxQjEyODZDNTBmOTFEMzBEYzQ3QjZFMDhmNDQwN0EiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0MjMxMTE4MDc5NywibmFtZSI6Im5mdGJldCJ9.OYctdmwHCX5YmRHTHtlkWcagIe3MMsHlREVW25eOVX8",
-});
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
-export async function storeMetadata() {
-  fs.readFile("../public/logo192.png", "utf-8", async (err, data) => {
-    if (err) throw err;
-    const url = await store(data);
-    console.log("Stored NFT successfully!\nMetadata URL: ", url);
+  Staking = await ethers.getContractFactory("StakingContract");
+
+  [owner, addr1, addr2] = await ethers.getSigners();
+  hardhatStaking = await Staking.deploy(addr1.address);
+  await hardhatStaking.deployed();
+
+  console.log("Staking Address ", hardhatStaking.address);
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
   });
-}
-
-async function store(data) {
-  // (3)
-  const fileCid = await client.storeBlob(new Blob([data]));
-  const fileUrl = "https://ipfs.io/ipfs/" + fileCid;
-
-  // (4)
-  const obj = {
-    name: "The Sample Text",
-    information: "This is a sample text file.",
-    creator: "Michelle Branagah",
-    file_url: fileUrl,
-  };
-
-  // (5)
-  const metadata = new Blob(JSON.stringify(obj), {
-    type: "application/json",
-  });
-  const metadataCid = await client.storeBlob(metadata);
-  const metadataUrl = "https://ipfs.io/ipfs/" + metadataCid;
-
-  return metadataUrl;
-}
